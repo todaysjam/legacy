4d59b8408ad2bd6e186fdebf77ccce0046a4641b
Hack Reactor Students <accounts@hackreactor.com>
2016-10-22 16:52:05 -0700

    (feat) Add recipe schema

--- db/models/recipeModel.js
+++ db/models/recipeModel.js
@@ -1,8 +1,33 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const RecipeSchema = new mongoose.Schema({
-//   q: String,
-//   uri: String,
-//   label: String,
-//   image: String,
-// });
+const IngredientSchema = new mongoose.Schema({
+  food: String,
+  quantity: Number,
+  measure: String,
+  weight: Number,
+});
+
+const NutrientSchema = new mongoose.Schema({
+  label: String,
+  tag: String,
+  hasRDI: Boolean,
+  quantity: Number,
+  unit: String,
+  daily: Number,
+});
+
+const RecipeSchema = new mongoose.Schema({
+  q: String,
+  uri: String,
+  label: String,
+  image: String,
+  servings: Number,
+  healthLabels: [String],
+  dietLabels: [String],
+  ingredients: [IngredientSchema],
+  calories: Number,
+  totalWeight: Number,
+  totalNutrients: [NutrientSchema],
+});
+
+module.exports = mongoose.model('recipe', RecipeSchema);
