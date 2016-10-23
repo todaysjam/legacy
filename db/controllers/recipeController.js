3d5311373b76a7f044ed96f8ad1fd431d4c77e3d
Cheney Mao <cheneymao94@gmail.com>
2016-10-23 01:57:09 -0700

    (feat) Query recipe database/API using promises

--- db/controllers/recipeController.js
+++ db/controllers/recipeController.js
@@ -1,25 +1,28 @@
-const Promise = require('bluebird');
 const Recipe = require('../models/recipeModel.js');
 const keys = require('../../server/config/config.js');
-const request = Promise.promisify(require('request'));
+const rp = require('request-promise');
 
 const APIURL = 'https://api.edamam.com/search?q=';
 
 module.exports = {
   findRecipes: (req, res) => {
     const query = req.params.q;
-    Recipe.find({ q: query }, (err, recipes) => {
-      if (recipes.length === 0) {
-        const queryString = `${APIURL}${query}&app_id=${keys.APP_ID}&app_key=${keys.APP_KEY}`;
-        request(queryString, (error, response, body) => {
-          if (error) {
-            res.status(404).send(err);
-          }
-          res.end(body);
-        });
-      } else {
-        res.end(recipes);
-      }
-    });
+    Recipe.find({ q: query }).exec()
+      .then((storedRecipes) => {
+        if (storedRecipes.length === 0) {
+          const queryString = `${APIURL}${query}&app_id=${keys.APP_ID}&app_key=${keys.APP_KEY}`;
+          rp(queryString)
+            .then((body) => {
+              const newRecipes = JSON.parse(body).hits
+                                 .map(recipeContainer => recipeContainer.recipe);
+              Recipe.create(newRecipes)
+                .then((recipes) => {
+                  res.json(recipes);
+                });
+            });
+        } else {
+          res.json(storedRecipes);
+        }
+      });
   },
 };
