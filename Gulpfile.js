649056a58c7fc23a46ca9f751ce3baa96bf61517
Cheney Mao <cheneymao94@gmail.com>
2016-10-23 22:15:26 -0700

    (test) Add test for adding recipe to db

---
+++ Gulpfile.js
@@ -0,0 +1,4 @@
+//'use strict';
+//const gulp = require('gulp');
+//const clean = require('gulp-clean');
+//const babel = require('gulp-babel');
--- tests/server/server-spec.js
+++ tests/server/server-spec.js
@@ -1,24 +1,42 @@
 /* You'll need to have MongoDB running and your Node server running
  * for these tests to pass. */
 const mocha = require('mocha');
-// const mongoose = require('mongoose');
-const request = require('request');
+const request = require('supertest');
 const expect = require('chai').expect;
+// const db = require('../../server/config/connection.js');
+const server = require('../../server/server.js');
+const Recipe = require('../../db/models/recipeModel.js');
 
 const describe = mocha.describe;
 const it = mocha.it;
-// const beforeEach = mocha.beforeEach;
+const beforeEach = mocha.beforeEach;
 // const afterEach = mocha.afterEach;
 
 describe('Node Recipe Server', () => {
-  it('Should insert posted recipes to the DB', (done) => {
-    request({
-      method: 'POST',
-      uri: 'http://127.0.0.1:3000/api/recipe',
-      json: { q: 'something' },
-    }, () => {
-      expect(true).to.equal(true);
-    });
+  const query = 'something';
+  beforeEach((done) => {
+    Recipe.remove({ q: query }).exec();
+
     done();
+  });
+
+
+  it('Should insert valid posted recipes to the DB', (done) => {
+    request(server)
+      .post('/api/recipe')
+      .send({
+        q: query,
+      })
+      .expect(200)
+      .expect(() => {
+        Recipe.find({ q: query })
+          .exec((err, recipes) => {
+            if (err) { console.err(err); }
+            expect(recipes.length).to.equal(1);
+            expect(recipes[0].q).to.equal(query);
+          });
+        // expect(1).to.equal(1);
+      })
+      .end(done);
   });
 });
