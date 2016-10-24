b98760935e6d539bfd722f04fe528a70a9098425
Cheney Mao <cheneymao94@gmail.com>
2016-10-24 11:42:21 -0700

    (test) Add tests for user schema

--- db/models/schema/userSchema.js
+++ db/models/schema/userSchema.js
@@ -4,10 +4,10 @@ const IngredientSchema = require('./ingredientSchema.js');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   meals: { type: Schema.Types.ObjectId, ref: 'Meal' },
-  shoppingList: { IngredientSchema },
+  shoppingList: [IngredientSchema],
 });
 
 module.exports = UserSchema;
--- tests/specs.js
+++ tests/specs.js
@@ -1,3 +1,4 @@
 'use strict';
 
-require('./server/server-spec.js');
+// require('./server/server-spec.js');
+require('./db/user-spec.js');
