6f0e72af9f2a22038a57aa4bf984a78c0d3032fc
rdgifford <robbie.gifford@gmail.com>
2016-10-26 16:01:30 -0700

    (feat) Prettify login screen with background image and styling.

--- mobile/app/Login.js
+++ mobile/app/Login.js
@@ -3,23 +3,47 @@ import {
   AsyncStorage,
   StyleSheet,
   Text,
+  Image,
   View,
   TouchableHighlight,
-  Alert
+  Alert,
 } from 'react-native';
 import t from 'tcomb-form-native';
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    // backgroundColor: 'black',
+  },
   container: {
     justifyContent: 'center',
-    marginTop: 50,
+    // alignItems: 'center',
+    marginTop: 30,
+    marginRight: 50,
+    marginLeft: 50,
+    borderRadius: 10,
+    flexDirection: 'column',
+    // width: 150,
+    // height: 300,
     padding: 20,
-    backgroundColor: '#ffffff',
+    backgroundColor: 'rgba(255,255,255,.75)',
   },
   title: {
-    fontSize: 30,
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginTop: 30,
+    backgroundColor: 'transparent',
     alignSelf: 'center',
     marginBottom: 30,
+    color: 'white',
+    opacity: 1,
+    textAlign: 'center',
+    textShadowColor: 'black',
+    textShadowRadius: 2,
+    textShadowOffset: {
+      width: 2,
+      height: 2,
+    },
   },
   buttonText: {
     fontSize: 18,
@@ -36,6 +60,9 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     justifyContent: 'center',
   },
+  backgroundImage: {
+    flex: 1,
+  }
 });
 // ENV Variables
 const loginUrl = 'https://mealdotnext4.herokuapp.com/api/user/authenticate';
@@ -84,33 +111,40 @@ export default class Login extends React.Component {
   }
   render() {
     return (
-    <View style={styles.container}>
-      <View style={styles.row}>
-        <Text style={styles.title}>Meal.next</Text>
-      </View>
-      <View style={styles.row}>
-        <Form
-          ref="form"
-          type={Person}
-          options={options}
-        />
-      </View>
-      <View style={styles.row}>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => this.authUser(loginUrl)}
-          underlayColor="limegreen"
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => this.authUser(signupUrl)}
-          underlayColor="limegreen"
-        >
-          <Text style={styles.buttonText}>Signup</Text>
-        </TouchableHighlight>
-      </View>
+    <View style={styles.main}>
+      <Image
+        source={{uri: 'http://s18.postimg.org/opemjpvrd/resized_background.jpg'}}
+        style={styles.backgroundImage}
+      >
+        <View style={styles.row}>
+          <Text style={styles.title}>Meal.next</Text>
+        </View>
+        <View style={styles.container}>
+          <View style={styles.row}>
+            <Form
+              ref="form"
+              type={Person}
+              options={options}
+            />
+          </View>
+          <View style={styles.row}>
+            <TouchableHighlight
+              style={styles.button}
+              onPress={() => this.authUser(loginUrl)}
+              underlayColor="limegreen"
+            >
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableHighlight>
+            <TouchableHighlight
+              style={styles.button}
+              onPress={() => this.authUser(signupUrl)}
+              underlayColor="limegreen"
+            >
+              <Text style={styles.buttonText}>Signup</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Image>
     </View>
     );
   }
