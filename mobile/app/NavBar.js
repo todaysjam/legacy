6a688b77f23202f5d563948455fcb8c5b6bb13e9
jeff christian <jchri@MacBook-Pro-2.loca>
2016-11-01 15:14:37 -0700

    navigation and header done

--- mobile/app/NavBar.js
+++ mobile/app/NavBar.js
@@ -1,23 +1,17 @@
 // https://github.com/facebook/react-native/blob/b998e5a7b74905b30b1137a02e14cd5e6f97fccc/Libraries/CustomComponents/Navigator/Navigator.js
 import React from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
-import Button from './Button';
 import MealList from './MealList';
 import ShoppingList from './ShoppingList';
 import AddMeal from './AddMeal';
+
+import { Container, Content, Footer, FooterTab, Button, Icon } from 'native-base';
 
 const width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   container: {
-    width,
-    height: 40,
-    backgroundColor: 'green',
-    borderTopWidth: 1,
-    borderColor: 'gray',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    marginTop: 0
   },
 });
 
@@ -28,11 +22,33 @@ const moveTo = (navigator, component) => {
 const NavBar = (props) => {
   if (props.navigator.getCurrentRoutes().length > 1) {
     return (
-      <View style={styles.container}>
-        <Button icon="md-list-box" onclick={() => moveTo(props.navigator, MealList)} />
-        <Button icon="md-basket" onclick={() => moveTo(props.navigator, ShoppingList)} />
-        <Button icon="md-add-circle" onclick={() => moveTo(props.navigator, AddMeal)} />
-      </View>
+       <Container style={styles.container}>
+                <Content />
+
+                <Footer >
+                    <FooterTab>
+                        <Button
+                          icon="md-list-box"
+                          onPress={() => moveTo(props.navigator, MealList)} 
+                          >
+                            Food
+
+                        </Button>
+                        <Button
+                          icon="md-basket"
+                          onPress={() => moveTo(props.navigator, ShoppingList)}
+                          >
+                            Calories
+                        </Button>
+                        <Button 
+                          icon="md-add-circle"
+                          onPress={() => moveTo(props.navigator, AddMeal)}
+                          >
+                            AddMeal
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
     );
   }
   return null;
