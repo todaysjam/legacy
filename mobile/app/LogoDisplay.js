8e5259466dcc527905ed22f2bfbf427b7f67c48a
jeff christian <jchri@MacBook-Pro-2.loca>
2016-11-01 15:45:55 -0700

    done with header

--- mobile/app/LogoDisplay.js
+++ mobile/app/LogoDisplay.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Dimensions, Text, StyleSheet } from 'react-native';
+import { View, Dimensions, Text, StyleSheet, Image } from 'react-native';
 import { Container, Content, Footer, FooterTab, Button, Icon, Header, Title } from 'native-base';
 
 const width = Dimensions.get('window').width;
@@ -7,7 +7,15 @@ const width = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   logo: {
     width:width,
-    marginBottom: 0
+    marginBottom: 0,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:5,
+    marginLeft: (width - 225)/2,
+
   },
   headline: {
     fontSize: 30,
@@ -16,12 +24,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0)',
     color: 'white',
   },
+  brand: {
+    width: 150,
+    height: 46
+  }
 });
 
 const LogoDisplay = () => (
   <Container style={styles.logo}>
         <Header>
-          <Title>Header</Title>
+          <Title style={styles.container}> <Image
+              // source={{uri: 'https://www.shopify.com/tools/logo-maker/show/YmRaSWk0YWVPenY4ZDh1NHAxMHBXdz09LS1GNUhZaDd2YTFMaHBHNmtGQUJqeWZBPT0=--2c33990e4f83acbff27c5ab8787c2f81e1187508.png'}}
+              source={require('../assets/brand.png')}
+              style={styles.brand}
+            >
+            </Image></Title>
       </Header>
   </Container>
 );
