8aa91a851fd1bcda91ada1b9238aac3fcd94a536
Tim Garibaldi <tgaribal@gmail.com>
2016-11-02 11:16:10 -0700

    Update server API key

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  APP_ID: '81375252',
-  APP_KEY: '05be9de10eb3e0cd8d671a2f7354c5e6',
+  APP_ID: 'aaad0688',
+  APP_KEY: '7e201929c6e4c0690d0d3efa46995399',
   secret: 'meatDotNextHahahahahaha',
 };
--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ const mongoUser = 'todaysjam';
 const mongoPassword = 'hr48';
 const mongoURI = `mongodb://${mongoUser}:${mongoPassword}@ds049466.mlab.com:49466/legacy`;
 mongoose.connect(mongoURI);
+// mongoose.connect('mongodb://localhost/legacy')
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
