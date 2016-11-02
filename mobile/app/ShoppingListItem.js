6fa3ea01b2e0c9b9b728d168b81a70a4cb4b9178
Tim Garibaldi <tgaribal@gmail.com>
2016-11-02 16:56:32 -0700

    Resolve rebase merge conflict

--- mobile/app/ShoppingListItem.js
+++ mobile/app/ShoppingListItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Dimensions } from 'react-native';
-import { Container, Content, List, ListItem, Text, CheckBox, AsyncStorage } from 'native-base';
+import { StyleSheet, Dimensions, AsyncStorage } from 'react-native';
+import { Container, Content, List, ListItem, Text, CheckBox } from 'native-base';
 
 const width = Dimensions.get('window').width;
 
@@ -23,21 +23,30 @@ export default class ShoppingListItem extends React.Component {
       checked: false
     }
   }
+  componentDidMount() {
+    AsyncStorage.getItem(this.props.item[3]).then( (value) => {
+      console.log('value', value)
+      this.setState({checked: value})
+      console.log('state', this.state.checked)
+    })
+  }
+
+  setCheck () {
+    AsyncStorage.setItem(this.props.item[3], JSON.stringify(!this.state.checked))
+    .then(console.log(!this.state.checked))
+    .then(console.log(AsyncStorage.getItem(this.props.item[3])))
+    .then(this.setState({checked: !this.state.checked}))
+    .then(console.log(this.state.checked))
+  }
 
   render() {
     return (
       <ListItem style={styles.table}>
         <CheckBox 
-          className=""
           checked={this.state.checked}
           onPress={this.setCheck}/>
         <Text>{this.props.item[0]} {this.props.item[1]} {this.props.item[2]}</Text>
       </ListItem>
     );
-  }
-
-  setCheck () {
-    this.setState({checked: !this.state.checked})
-  }
-  
+  } 
 }
