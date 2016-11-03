bc91906df274b847625413f2f805e7c2967bb775
Sherman <shc410@gmail.com>
2016-11-02 20:25:50 -0700

    added swipeout library and fix import issue, added swipeout functionality to loggedMeal screen

--- mobile/app/LoggedMeal.js
+++ mobile/app/LoggedMeal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, Dimensions, Text, StyleSheet, TouchableHighlight } from 'react-native';
 import { Container, Content, Footer, FooterTab, Button, Icon, Header, Title } from 'native-base';
+import Swipeout from 'react-native-swipeout';
 
 const width = Dimensions.get('window').width;
 
@@ -40,30 +41,39 @@ const styles = StyleSheet.create({
   }
 });
 
+const swipeoutBtns = [
+  {
+    text: 'Button'
+  }
+];
+
+
 const LoggedMeal = ({ recipe, mealId, showInfo }) => (
-  <TouchableHighlight
-    style={styles.tile}
-    onPress={() => showInfo(recipe, mealId)}
-  >
-    <View style={styles.itemContainer}>
-      <Image
-        style={styles.picture}
-        source={{ uri: recipe.image }}
-        >
-      </Image>
-      <View >
-        <Text style={styles.headline}>
-          {recipe.label}
-        </Text>
-        <Text style={styles.headline}>
-          {Math.round(recipe.calories) + ' cal'}
-        </Text>
-        <Text style={styles.headline}>
-          {Math.round(recipe.totalWeight) + ' g'}
-        </Text>
+  <Swipeout right={swipeoutBtns}>
+    <TouchableHighlight
+      style={styles.tile}
+      onPress={() => showInfo(recipe, mealId)}
+    >
+      <View style={styles.itemContainer}>
+        <Image
+          style={styles.picture}
+          source={{ uri: recipe.image }}
+          >
+        </Image>
+        <View >
+          <Text style={styles.headline}>
+            {recipe.label}
+          </Text>
+          <Text style={styles.headline}>
+            {Math.round(recipe.calories) + ' cal'}
+          </Text>
+          <Text style={styles.headline}>
+            {Math.round(recipe.totalWeight) + ' g'}
+          </Text>
+        </View>
       </View>
-    </View>
-  </TouchableHighlight>
+    </TouchableHighlight>
+  </Swipeout>
 );
 
 export default LoggedMeal;
