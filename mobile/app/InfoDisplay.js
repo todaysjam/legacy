0e3b9b00c384e8bc9c30c4a181f729afeae8ef4d
Tim Garibaldi <tgaribal@gmail.com>
2016-11-03 17:39:01 -0700

    Resolve git conflict

--- mobile/app/InfoDisplay.js
+++ mobile/app/InfoDisplay.js
@@ -56,60 +56,82 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   chart: {
-      width: 200,
-      height: 200,
+      width: width * 0.75,
+      height: 300,
   },
 });
 
-/* eslint-disable no-param-reassign */
-const compileNutrition = (data) => {
-  const result = [];
-  data.forEach((item, i) => {
-    if(i < 6){
-      result.push(item);
-      if (item.sub) {
-        item.sub.forEach((subItem) => {
-          subItem.label = ` ${subItem.label}`;
-          result.push(subItem);
-        });
+export default class InfoDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   data: []  
+    // }
+  }
+
+  /* eslint-disable no-param-reassign */
+  compileNutrition(data) {
+    const result = [];
+    data.forEach((item, i) => {
+      if(i < 6){
+        result.push(item);
+        if (item.sub) {
+          item.sub.forEach((subItem) => {
+            subItem.label = ` ${subItem.label}`;
+            result.push(subItem);
+          });
+        } 
       }
-    }
-  });
-  result.forEach((item) => {
-    const totalUnit = Math.round(Number(Number(item.total).toFixed(2)).toString()) + item.unit;
-    item.totalUnit = totalUnit;
-    const dailyPercent = `${Math.round(Number(Number(item.daily).toFixed(1)).toString())}%`;
-    item.dailyPercent = dailyPercent;
-  });
-  return result;
-};
+    });
+    result.forEach((item) => {
+      const totalUnit = Math.round(Number(Number(item.total).toFixed(2)).toString()) + item.unit;
+      item.totalUnit = totalUnit;
+      const dailyPercent = `${Math.round(Number(Number(item.daily).toFixed(1)).toString())}%`;
+      item.dailyPercent = dailyPercent;
+    });
+    return result;
+  }
 /* eslint-enable no-param-reassign */
-//423
-const data = [
-    [0, 148.08104399999996], //35%
-    [148.08104399999996, 40.361999999999995], //9.5%
-    [40.361999999999995, 235.76718799999998] //55
-];
 
-const getMacro = (props, macro) => {
-  compileNutrition(props.recipe.digest.filter ((item) => {
-    if (item.label === macro) {
-      console.log('item', item)
-    }
-  }))
+  getMacro (macro) {
+    this.compileNutrition(this.props.recipe.digest
+      .filter(item => item.label === macro)
+      .map(nutrient => nutrient.total)
+    )
+  }
 
-}
+  // componentDidMount() {
+  //   const fat = Math.round(this.getMacro('Fat'))
+  //   const carbs = Math.round(this.getMacro('Carbs'))
+  //   const protein = Math.round(this.getMacro('Protein'))
+  //   this.setState({data: [[0, fat], [fat, carbs], [carbs, protein]]});
+  // }
 
-const InfoDisplay = props => (
-  <View style={styles.container}>
-    <HeadBuffer />
-    <HeaderDisplay recipe={props.recipe} />
-    <ScrollView contentContainerStyle={styles.scroller}>
-      <Image
-        style={styles.picture}
-        source={{ uri: props.recipe.image }}
-      />
-      
+  render() {
+    return (
+      <View style={styles.container}>
+        <HeadBuffer />
+        <HeaderDisplay recipe={this.props.recipe} />
+        <ScrollView contentContainerStyle={styles.scroller}>
+          <Image
+            style={styles.picture}
+            source={{ uri: this.props.recipe.image }}
+          />
+          <View style={styles.buttonContainer}>
+            <Button
+              onclick={() => { this.props.postMeal(this.props.recipe._id, this.props.mealId); }} // eslint-disable-line
+              text={this.props.text}
+            />
+            <Button
+              onclick={() => { this.props.navigator.pop(); }}
+              text="Back"
+            />
+          </View>
+          
+          <View style={styles.table}>
+            <Text style={styles.caloriesB}>Calories:</Text>
+            <Text style={styles.calories}>{Math.round(this.props.recipe.calories)}</Text>
+          </View>
 
       <View style={styles.buttonContainer}>
         <Button
@@ -128,59 +150,65 @@ const InfoDisplay = props => (
         <Text style={styles.caloriesB}>Calories:</Text>
         <Text style={styles.calories}>{Math.round(props.recipe.calories)}</Text>
       </View>
+          <View style={styles.table}>
+            <Column
+              data={this.props.recipe.ingredients}
+              name="Ingredients"
+              index="food"
+            />
+            <Column
+              data={this.props.recipe.ingredients}
+              name="Qty"
+              index="quantity"
+              alignRight
+            />
+            <Column
+              data={this.props.recipe.ingredients}
+              name="Unit"
+              index="measure"
+              alignRight
+            />
+          </View>
 
-      <View style={styles.table}>
-        <Column
-          data={props.recipe.ingredients}
-          name="Ingredients"
-          index="food"
-        />
-        <Column
-          data={props.recipe.ingredients}
-          name="Qty"
-          index="quantity"
-          alignRight
-        />
-        <Column
-          data={props.recipe.ingredients}
-          name="Unit"
-          index="measure"
-          alignRight
-        />
+          <View style={styles.table}>
+            <Column
+              data={this.compileNutrition(this.props.recipe.digest)}
+              name="Nutrition"
+              index="label"
+            />
+            <Column
+              data={this.compileNutrition(this.props.recipe.digest)}
+              name="Qty"
+              index="totalUnit"
+              alignRight
+            />
+            <Column
+              data={this.compileNutrition(this.props.recipe.digest)}
+              name="Daily%"
+              index="dailyPercent"
+              alignRight
+            />
+          </View>
+
+          <View style={styles.container}>
+              <Chart
+                  style={styles.chart}
+                  // data={this.state.data}
+                  data={[
+                    [0, this.getMacro('Fat')],
+                    [this.getMacro('Fat'), this.getMacro('Carbs')],
+                    [this.getMacro('Carbs'), this.getMacro('Protein')]
+                  ]}
+                  type="pie"
+                  showDataPoint={true}
+               />
+          </View>
+
+        </ScrollView>
       </View>
+    );
+  }
+}
 
-      <View style={styles.table}>
-        <Column
-          data={compileNutrition(props.recipe.digest)}
-          name="Nutrition"
-          index="label"
-        />
-        <Column
-          data={compileNutrition(props.recipe.digest)}
-          name="Qty"
-          index="totalUnit"
-          alignRight
-        />
-        <Column
-          data={compileNutrition(props.recipe.digest)}
-          name="Daily%"
-          index="dailyPercent"
-          alignRight
-        />
-      </View>
-      <View style={styles.container}>
-          <Chart
-              style={styles.chart}
-              data={data}
-              type="pie"
-              showDataPoint={true}
-           />
-      </View>
-
-    </ScrollView>
-  </View>
-);
-
-export default InfoDisplay;
 
 
