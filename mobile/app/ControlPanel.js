3d72473c3cc62ad0a495ee5bff10771dce453be5
jeff christian <jchri@MacBook-Pro-2.loca>
2016-11-05 11:21:35 -0700

    update;

--- mobile/app/ControlPanel.js
+++ mobile/app/ControlPanel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { PropTypes, ScrollView, StyleSheet, Text, TextInput, Dimensions, View } from 'react-native'
 const width = Dimensions.get('window').width;
 import { Button } from 'native-base';
+import { Button, Radio, List, ListItem, Container, Content } from 'native-base'
 
 export default class ControlPanel extends Component {
   // static propTypes = {
@@ -9,34 +10,115 @@ export default class ControlPanel extends Component {
   // };
   constructor(props) {
     super(props);
+    this.onGenderSelect = this.onGenderSelect.bind(this);
+    this.Calculate = this.Calculate.bind(this);
     this.state={
-      text: ''
+      text: '',
+      age: '',
+      weight: '',
+      feet: '',
+      Inches: '',
+      gender: 'male'
     }
   }
+  onGenderSelect(e){
+    this.setState({
+      gender: e.currentTarget.value
+    })
+  }
+  Calculate(){
 
+<<<<<<< 823a9e65c15dad06e49689b9dcb7885236f15233
   changeGlobalCalories() {
     global._globalCaloriesCount = parseInt(this.state.text);
     this.props.updateCalories();
     console.log('global calories: ', global._globalCaloriesCount);
   }
 
+=======
+  }
+>>>>>>> drawer adds
   render() {
     return (
       <ScrollView style={styles.container}>
-        <Text>Enter Your Target Calories</Text>
-        <TextInput
-        placeholder='Ex: 14000'    
+        <Text>Enter Your Calorie Info</Text>
+         <TextInput
+        placeholder='Enter Age'    
+        style={{height: 40, width: width * .4, color: 'black', marginLeft: 5}}
+        placeholderTextColor='gray'
+        underlineColorAndroid='gray'
+        />
+         <TextInput
+        placeholder='Enter Weight'    
         style={{height: 40, width: width * .4, color: 'black', marginLeft: 5}}
         onChangeText={text => this.setState({ text: text })}
         placeholderTextColor='gray'
         underlineColorAndroid='gray'
         />
+        <Text> Enter Height</Text>
+        <TextInput
+        placeholder='Feet'    
+        style={{height: 40, width: width * .2, color: 'black', marginLeft: 5}}
+        textStyle={{fontSize: 12}}
+        placeholderTextColor='gray'
+        underlineColorAndroid='gray'
+        />
+        <TextInput
+        placeholder='Inches'    
+        style={{height: 40, width: width * .2, color: 'black', marginLeft: 5}}
+        textStyle={{fontSize: 12}}
+        placeholderTextColor='gray'
+        underlineColorAndroid='gray'
+        />
+        <Text>Gender</Text>
+        <Container>
+          <Content>
+          <List>
+            <ListItem>
+              <Radio
+                selected={true}
+                value='male'
+                onPress={this.onGenderSelect}
+              />
+              <Text>Male</Text>
+            </ListItem>
+            <ListItem>
+              <Radio
+              selected={false}
+              value='female'
+              onPress={this.onGenderSelect}
+              />
+              <Text>Female</Text>
+            </ListItem>
+          </List>
+          </Content>
+        </Container>
         <View>
           <Button
-            style={{ width: width * .4}}
+            style={{ width: width * .4, height: 20, backgroundColor: 'gray'}}
             textStyle={{color: 'white',fontWeight: 'bold', fontSize: 20}}
+            rounded  
+            onPress={() => this.props.enter(this.state.text)}
+            >
+            Calculate
+          </Button>
+          <Text style={{marginTop: 20}}>Reccomended Calories</Text>
+          <TextInput
+            placeholder='Ex:14000'    
+            style={{height: 40, width: width * .4, color: 'black', marginLeft: 5}}
+            placeholderTextColor='gray'
+            underlineColorAndroid='gray'
+            />
+          <Button
+            style={{ width: width * .4, height: 30}}
+            textStyle={{color: 'white',fontWeight: 'bold', fontSize: 20}}
+<<<<<<< 823a9e65c15dad06e49689b9dcb7885236f15233
             rounded block 
             onPress={() => this.changeGlobalCalories()}
+=======
+            rounded  
+            onPress={() => this.props.enter(this.state.text)}
+>>>>>>> drawer adds
             >
             Submit
           </Button>
