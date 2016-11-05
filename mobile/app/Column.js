b55970d04e635e8e52696a5a46cedad373d448c0
Sherman <shc410@gmail.com>
2016-11-05 11:32:10 -0700

    code reorg in column.js

--- mobile/app/Column.js
+++ mobile/app/Column.js
@@ -1,21 +1,9 @@
 import React from 'react';
+
+// import packages
 import { View, Text, StyleSheet } from 'react-native';
 
-// stylesheet
-const styles = StyleSheet.create({
-  columnLeft: {
-
-  },
-  columnRight: {
-    alignItems: 'flex-end',
-  },
-  head: {
-    textDecorationLine: 'underline',
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
-}); // end styles
-
+// Column Component
 const Column = ({ data, name, index, alignRight }) => (
   <View style={alignRight ? styles.columnRight : styles.columnLeft}>
     <Text style={styles.head}>
@@ -33,6 +21,21 @@ const Column = ({ data, name, index, alignRight }) => (
       </Text>
     ))}
   </View>
-); // end Column
+); // end Column Component
+
+// stylesheet
+const styles = StyleSheet.create({
+  columnLeft: {
+
+  },
+  columnRight: {
+    alignItems: 'flex-end',
+  },
+  head: {
+    textDecorationLine: 'underline',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+}); // end styles
 
 export default Column;
