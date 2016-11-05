8046f5059acf38a9458b333ae4be94130f278deb
Sherman <shc410@gmail.com>
2016-11-05 11:39:55 -0700

    code reorg in infodisplay.js, loggedmeal.js

--- mobile/app/LoggedMeal.js
+++ mobile/app/LoggedMeal.js
@@ -1,12 +1,76 @@
 import React from 'react';
+
+// import packages
 import { View, Image, Dimensions, Text, StyleSheet, TouchableHighlight } from 'react-native';
 import { Container, Content, Footer, FooterTab, Button, Icon, Header, Title } from 'native-base';
 import Swipeout from 'react-native-swipeout';
 
+// establish constants
 const width = Dimensions.get('window').width;
 const mealUrl = 'https://mealdotlegacy.herokuapp.com/api/meal/';
 
-// stylesheet definition
+// LoggedMeal Component
+export default class LoggedMeal extends React.Component {
+  constructor(props) {
+    super(props);
+  } //end constructor
+
+  //define a function to fire the ajax request to delete a meal from the mealList of a user
+  postMeal(mealId, token) {
+    fetch(mealUrl + mealId, {
+      method: 'DELETE',
+      headers: { 'x-access-token': token },
+    })
+    .then(() => {
+      this.props.updateMeals();
+    })
+  } //end PostMeal
+
+  render() {
+    // define the clickable button shown when an entry of meal is swiped
+    const swipeoutBtns = [
+      {
+        text: 'Delete',
+        backgroundColor: 'red',
+        underlayColor: 'transparent',
+        color: 'white',
+        onPress: () => {
+          this.postMeal(this.props.mealId, this.props.token);
+        }
+      }
+    ]; // end swipeoutBtns
+
+    return (
+      <Swipeout right={swipeoutBtns} autoClose={true} >
+        <TouchableHighlight
+          style={styles.tile}
+          onPress={() => this.props.showInfo(this.props.recipe, this.props.mealId)}
+        >
+          <View style={styles.itemContainer}>
+            <Image
+              style={styles.picture}
+              source={{ uri: this.props.recipe.image }}
+              >
+            </Image>
+            <View >
+              <Text style={styles.headline}>
+                {this.props.recipe.label}
+              </Text>
+              <Text style={styles.headline}>
+                {Math.round(this.props.recipe.calories) + ' cal'}
+              </Text>
+              <Text style={styles.headline}>
+                {Math.round(this.props.recipe.totalWeight) + ' g'}
+              </Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </Swipeout>
+    );
+  } //end render
+} // end LoggedMeal Component
+
+// stylesheet
 const styles = StyleSheet.create({
   tile: {
     width: width * 0.9,
@@ -40,64 +104,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
   }
-}); //end of styles definition
-
-export default class LoggedMeal extends React.Component {
-  constructor(props) {
-    super(props);
-  } //end of constructor
-
-  //define a function to fire the ajax request to delete a meal from the mealList of a user
-  postMeal(mealId, token) {
-    fetch(mealUrl + mealId, {
-      method: 'DELETE',
-      headers: { 'x-access-token': token },
-    })
-    .then(() => {
-      this.props.updateMeals();
-    })
-  } //end of function: PostMeal
-
-  render() {
-    // define the clickable button shown when an entry of meal is swiped
-    const swipeoutBtns = [
-      {
-        text: 'Delete',
-        backgroundColor: 'red',
-        underlayColor: 'transparent',
-        color: 'white',
-        onPress: () => {
-          this.postMeal(this.props.mealId, this.props.token);
-        }
-      }
-    ]; // end of clickable button defintion 
-
-    return (
-      <Swipeout right={swipeoutBtns} autoClose={true} >
-        <TouchableHighlight
-          style={styles.tile}
-          onPress={() => this.props.showInfo(this.props.recipe, this.props.mealId)}
-        >
-          <View style={styles.itemContainer}>
-            <Image
-              style={styles.picture}
-              source={{ uri: this.props.recipe.image }}
-              >
-            </Image>
-            <View >
-              <Text style={styles.headline}>
-                {this.props.recipe.label}
-              </Text>
-              <Text style={styles.headline}>
-                {Math.round(this.props.recipe.calories) + ' cal'}
-              </Text>
-              <Text style={styles.headline}>
-                {Math.round(this.props.recipe.totalWeight) + ' g'}
-              </Text>
-            </View>
-          </View>
-        </TouchableHighlight>
-      </Swipeout>
-    );
-  } //end of function: render
-}
+}); //end styles
